c23de83693f5f82fa6e40bea6f1cc553f59cdb64
Jason <quizathaderat@yahoo.com>
2015-01-20 12:42:31 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoBooth::Application.config.secret_token = '79e957507e95acf13f3491bfc9382a057113616847aecbfce775a6b94517c76eefa1ae1bd13730f48b7570a95e8489eae82226e8da545d49a373a680f51579b2'
