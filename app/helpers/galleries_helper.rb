2d65ee14edd54943e0d006d3b7de195f3569ea58
Jason <quizathaderat@yahoo.com>
2015-01-22 15:44:49 -0800

    Galleries have random photo covers

--- app/helpers/galleries_helper.rb
+++ app/helpers/galleries_helper.rb
@@ -1,11 +1,21 @@
 module GalleriesHelper
 	def random_photos_array
-		if @galleries
-			photos = Array.new
-			@galleries.find(rand(1) + 1).photos.each do |photo|
-			  photos.push(photo)
-		  end
-		  photos
+		photos = Array.new
+		@galleries.find(rand(@galleries.length) + 1).photos.each do |photo|
+		  photos.push(photo)
 	  end
+	  photos
 	end
+
+	def random_gallery_cover(gallery)
+		photos = Array.new
+		gallery.photos.each do |photo|
+		  photos.push(photo)
+		end
+		photos[rand(10) + 1].image_url(:thumb).to_s 
+	end
+
+
 end
+
+
