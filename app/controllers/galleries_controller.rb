a653fb0d07baebbcbfd6939ed82d1e67ede98fc0
Jason <quizathaderat@yahoo.com>
2015-02-07 20:17:38 -0800

    Gallery create action set first gallery to id 1 and subsequent galleries to subsequent ids

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -29,14 +29,15 @@ class GalleriesController < ApplicationController
 	end
 
 	def create
+		first = true if Gallery.all.length == 0
 		@gallery = Gallery.new(gallery_params)
-		if Gallery.all.length == 0
-		  @gallery.id = 1 
-		else
-		  @gallery.id = Gallery.last.id.to_i + 1 
-		end
+		@gallery.id = Gallery.last.id.to_i + 1 unless first == true
 		respond_to do |format|
 			if @gallery.save
+				if first == true
+		      @gallery.id = 1 
+		      @gallery.save 
+		    end
 				format.html { redirect_to galleries_path, notice: 'Gallery has been created' }
 				format.json { head :no_content }
 			else
